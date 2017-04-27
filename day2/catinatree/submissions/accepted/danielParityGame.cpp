d5920ea29319ecc4c1ef3bafd1e22e7f5a65cf89
Anton Grensjö <anton@grensjo.se>
2017-04-27 11:48:47 +0200

    Added new submission for catinatree

---
+++ day2/catinatree/submissions/accepted/danielParityGame.cpp
@@ -0,0 +1,25 @@
+#include<cstdio>
+#include<vector>
+
+using namespace std;
+
+
+int main() {
+	char inp[150000];
+	scanf("%140000s", &inp);
+	
+	string s = inp;
+	cout << s << endl;
+
+
+
+}
+	
+	
+	
+	
+	
+	
+	
+	
+}
